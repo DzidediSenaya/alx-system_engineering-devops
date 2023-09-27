140028f0ea6bc0c2008531fac12a03aa2813647c
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-09-27 18:57:36 +0000

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Puppet manifest to create a file in /tmp with specific permissions and content
 
 file { '/tmp/school':
-  ensure   => 'file',
-  mode     => '0744',
-  owner    => 'www-data',
-  group    => 'www-data',
-  content  => 'I love Puppet',
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
