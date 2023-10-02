2a70ca20510a6d76c86aa1c2cb336bb5badd9a33
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-10-02 23:14:36 +0000

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,35 +1,37 @@
 #!/usr/bin/env bash
-# Puppet manifest to configure a custom HTTP response header for Nginx
+# This script configures Nginx to include a custom HTTP response header
+# named X-Served-By with the value being the hostname of the server.
 
-# Define a custom fact to retrieve the server's hostname
-Facter.add(:custom_hostname) do
-  setcode 'hostname -f'
-end
+# Check if the script is being run with superuser privileges
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run this script as root or using sudo."
+    exit 1
+fi
 
-# Install Nginx package
-package { 'nginx':
-  ensure => installed,
-}
+# Install Nginx if not already installed
+if ! command -v nginx &>/dev/null; then
+    apt-get update
+    apt-get install -y nginx
+fi
 
 # Create a custom Nginx configuration file
-file { '/etc/nginx/conf.d/custom_response_header.conf':
-  ensure  => present,
-  content => "server {
+echo "Adding custom Nginx configuration..."
+cat <<EOF > /etc/nginx/conf.d/custom_response_header.conf
+server {
     listen 80;
     server_name _;
 
     location / {
-        add_header X-Served-By $::custom_hostname;
+        add_header X-Served-By \$(hostname);
         # Other Nginx configuration directives can go here
     }
-}\n",
-  require => Package['nginx'],
-  notify  => Service['nginx'],
 }
+EOF
 
-# Ensure Nginx service is running
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => Package['nginx'],
-}
+# Test Nginx configuration and reload if successful
+if nginx -t; then
+    systemctl reload nginx
+    echo "Nginx configuration reloaded."
+else
+    echo "Nginx configuration test failed. Please check your configuration."
+fi
