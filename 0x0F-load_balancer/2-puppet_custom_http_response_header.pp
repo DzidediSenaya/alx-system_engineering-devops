df625575bce23898923d71fde4444a609088d6b8
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-10-04 00:52:17 +0000

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,37 +1,18 @@
 #!/usr/bin/env bash
 # This script configures Nginx to include a custom HTTP response header
-# named X-Served-By with the value being the hostname of the server.
 
-# Check if the script is being run with superuser privileges
-if [ "$EUID" -ne 0 ]; then
-    echo "Please run this script as root or using sudo."
-    exit 1
-fi
-
-# Install Nginx if not already installed
-if ! command -v nginx &>/dev/null; then
-    apt-get update
-    apt-get install -y nginx
-fi
-
-# Create a custom Nginx configuration file
-echo "Adding custom Nginx configuration..."
-cat <<EOF > /etc/nginx/conf.d/custom_response_header.conf
-server {
-    listen 80;
-    server_name _;
-
-    location / {
-        add_header X-Served-By \$(hostname);
-        # Other Nginx configuration directives can go here
-    }
+# custom http header response NGiNX
+exec {'update':
+  command => '/usr/bin/apt-get update',
 }
-EOF
-
-# Test Nginx configuration and reload if successful
-if nginx -t; then
-    systemctl reload nginx
-    echo "Nginx configuration reloaded."
-else
-    echo "Nginx configuration test failed. Please check your configuration."
-fi
+-> package {'nginx':
+  ensure => 'present',
+}
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+}
+-> exec {'run2':
+  command => '/usr/sbin/service nginx start',
+}
