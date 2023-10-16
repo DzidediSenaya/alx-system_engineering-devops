cb16c96ad06331b1bb6ceb560805be5bbff3864b
DzidediSenaya <dzidedisenaya@gmail.com>
2023-10-16 15:27:58 -0700

    finished all tasks

---
+++ 0x0F-load_balancer/copy_script_to_servers.sh
@@ -0,0 +1,5 @@
+# Copy the script to web-01
+scp /path/to/your/script.sh username@52.87.211.229:/path/to/remote/directory
+
+# Copy the script to web-02
+scp /path/to/your/script.sh username@54.162.222.32:/path/to/remote/directory
