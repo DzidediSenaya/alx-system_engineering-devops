0eb6aa596e788db319fc8673174e5d59e3b0f422
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-10-23 22:49:57 +0000

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,57 +9,15 @@ import sys
 import csv
 
 
-def get_employee_todo_progress(employee_id):
-    # API URL to retrieve employee information
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_url = f"{base_url}/users/{employee_id}"
-
-    # Fetch employee data
-    response = requests.get(user_url)
-    employee_data = response.json()
-
-    # Fetch the user's TODO list
-    todo_url = f"{base_url}/todos?userId={employee_id}"
-    response = requests.get(todo_url)
-    todo_list = response.json()
-
-    # Prepare CSV file name
-    user_id = employee_data['id']
-    file_name = f"{user_id}.csv"
-
-    # Define the header row for the CSV file
-    header_row = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-
-    # Open and write to the CSV file
-    with open(file_name, mode='w', newline='') as file:
-        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-        writer.writerow(header_row)
-
-        # Write the data rows, sorting dictionaries alphabetically
-        for task in sorted(todo_list, key=lambda x: x['title']):
-            writer.writerow([user_id, employee_data['username'],
-                             str(task['completed']), task['title']])
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 1-export_to_CSV.py <employee_id>")
-        sys.exit(1)
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    employee_id = int(sys.argv[1])
-    get_employee_todo_progress(employee_id)
-
-    # Check and print the number of tasks in the CSV
-    with open(f"{employee_id}.csv") as csv_file:
-        csv_reader = csv.reader(csv_file)
-        num_tasks = sum(1 for row in csv_reader) - 1  # Subtract 1 to exclude the header row
-        print(f"Number of tasks in CSV: {num_tasks}")
-
-    # Check and print user ID and username
-    if num_tasks > 0:
-        with open(f"{employee_id}.csv") as csv_file:
-            csv_reader = csv.reader(csv_file)
-            next(csv_reader)  # Skip the header row
-            first_row = next(csv_reader)
-            user_id, username, _, _ = first_row
-            print(f"User ID: {user_id} / Username: {username}")
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
