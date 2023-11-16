c6c4a4425ee057406bcf6d50770487d876cbaf1a
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-11-16 22:15:12 +0000

    Update 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,23 +1,13 @@
-# Puppet Manifest to Increase File Limits for holberton user
+# Enable the user holberton to login and open files without error.
 
-# Exec resource to set file limits for holberton user
-exec { 'set-file-limits-for-holberton':
-  command => 'echo "holberton soft nofile 4096" >> /etc/security/limits.conf && echo "holberton hard nofile 4096" >> /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/',
-  onlyif  => 'test ! -e /etc/security/limits.d/20-nproc.conf', # Check if not already set
+# Increase hard file limit for Holberton user.
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
 
-# Exec resource to apply the changes
-exec { 'apply-file-limits':
-  command => 'echo "ulimit -n 4096" >> /home/holberton/.bashrc',
-  path    => '/usr/local/bin/:/bin/',
-  onlyif  => 'grep "holberton" /etc/security/limits.conf', # Check if user limit is set
-  require => Exec['set-file-limits-for-holberton'], # Ensure the limits are set before applying
-}
-
-# Exec resource to restart the session for changes to take effect
-exec { 'restart-session':
-  command     => 'su - holberton -c "source ~/.bashrc"',
-  refreshonly => true,
-  subscribe   => Exec['apply-file-limits'],
+# Increase soft file limit for Holberton user.
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
