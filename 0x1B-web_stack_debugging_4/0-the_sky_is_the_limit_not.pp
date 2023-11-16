04b715323f06151fe1776cca2e73d14eb6a33211
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-11-16 22:18:34 +0000

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,29 @@
-# Increases the amount of traffic an Nginx server can handle.
+# Puppet Manifest to Increase Worker Connections in Nginx Configuration
 
-# Increase the ULIMIT of the default file
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-} ->
+# Increase worker connections in Nginx configuration
+exec { 'fix-for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/nginx/nginx.conf',
+  path    => '/usr/local/bin/:/bin/',
+  notify  => Exec['nginx-restart'],
+}
 
-# Restart Nginx
+# Restart Nginx after configuration changes
 exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+  command     => 'service nginx restart',
+  path        => '/etc/init.d/',
+  refreshonly => true,
+}
+
+# Sleep for a few seconds to allow Nginx to fully restart before continuing
+exec { 'sleep':
+  command => 'sleep 5',
+  path    => '/bin/',
+  before  => Exec['ab-benchmark'],
+}
+
+# ApacheBench benchmarking
+exec { 'ab-benchmark':
+  command => 'ab -n 2000 -c 100 http://localhost/',
+  path    => '/usr/bin/',
+  require => Exec['sleep'],
 }
