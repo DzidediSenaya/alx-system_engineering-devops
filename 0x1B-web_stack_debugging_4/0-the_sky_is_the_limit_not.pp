140224e91234d0513413e45a6b2ee751273d16c7
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-11-16 22:03:35 +0000

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,25 +1,14 @@
 # Puppet Manifest to Increase Worker Connections in Nginx Configuration
 
-# Define a Puppet class to encapsulate the configuration changes
-class nginx_worker_connections {
-  
-  # Exec resource to replace the value '15' with '4096' in the nginx default configuration
-  exec { 'fix--for-nginx':
-    command => 'sed -i "s/15/4096/" /etc/default/nginx',
-    path    => '/usr/local/bin/:/bin/',
-    # Notify the 'nginx-restart' exec resource to run if changes are made by this exec
-    notify  => Exec['nginx-restart'],
-  }
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
 
   # Exec resource to restart Nginx after configuration changes
   exec { 'nginx-restart':
-    command => 'service nginx restart',
-    path    => '/etc/init.d/',
-    # Only run if notified by the 'fix--for-nginx' exec resource
-    refreshonly => true,
-  }
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
-
 # Apply the class to the node
 include nginx_worker_connections
-
