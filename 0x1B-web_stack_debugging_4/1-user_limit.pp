0b5ed4941b8340cb7a4f99f169b8e5192df4d13e
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-11-16 22:31:36 +0000

    Update 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,13 @@
 # Puppet manifes to allow seamless login for holberton user and error-free file opening.
 
 # Increase hard file limit for Holberton user.
-exec { 'increase-hard-file-limit-for-holberton-user':
+exec { 'fix--for-nginx':
   command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
 
 # Increase soft file limit for Holberton user.
-exec { 'increase-soft-file-limit-for-holberton-user':
+exec { 'nginx-restart':
   command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
