731aa43e23e6ac5ba574de8de49e26840ad03ca2
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-11-16 22:08:08 +0000

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,15 @@
 # Puppet Manifest to Increase Worker Connections in Nginx Configuration
 
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-} ->
+# Increase worker connections in Nginx configuration
+exec { 'fix-for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/nginx/nginx.conf',
+  path    => '/usr/local/bin/:/bin/',
+  notify  => Exec['nginx-restart'],
+}
 
 # Restart Nginx after configuration changes
 exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+  command => 'service nginx restart',
+  path    => '/etc/init.d/',
+  refreshonly => true,
 }
